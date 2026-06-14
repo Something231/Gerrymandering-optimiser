d558555ca1e062fd1b409683f88bf7eb3cb31992
Something231 <119870385+Something231@users.noreply.github.com>
2026-06-14 17:28:06 +0800

    basic assign votes logic

---
+++ DistrictLogic.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+#mean is a 2d vector, covariance is a 2x2 matrix
+class Cell:
+    def __init__(self, mean, covariance, population:int):
+        self.mean = mean
+        self.covariance = covariance
+        self.population:int = population
+        self.people = np.random.multivariate_normal(self.mean, self.covariance, self.population)
+        self.explode = True
+        self.votes:list
+
+    def assign_votes(self, parties):
+        self.votes = [0] * len(parties)
+        for p in self.people:
+            closest_distance = float(99999.9)
+            closest_id = None
+            for n, q in enumerate(parties):
+                d = np.linalg.norm(q - p)
+                if d == closest_distance:
+                    exit()
+                elif d < closest_distance:
+                    closest_distance = d
+                    closest_id = n
+            self.votes[n] += 1
