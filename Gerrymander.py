a6068a41a35b40e715c442f61c17cd9a6e2a491a
Something231 <119870385+Something231@users.noreply.github.com>
2026-06-15 20:05:41 +0800

    just make it so that the districts can be viewed visually

---
+++ Gerrymander.py
@@ -0,0 +1,14 @@
+import numpy as np
+from math import ceil
+
+def verify_possible(districts, est_tally, e_size, w) -> bool:  #quick check to see weather it is reasonably doable, will miss some chances but whatever
+    size = districts.shape
+    t = size[0]*size[1]
+    n = int(t/e_size)
+    if est_tally[w] >= ((ceil(0.51 * n)/n) * (ceil(0.51 * e_size)/e_size) * t):
+        return True
+    else:
+        return False
+
+def simple_attempt(districts):
+    pass
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from DistrictLogic import Cell
 import numpy as np
 import random
+import matplotlib.pyplot
+import matplotlib.patches
 
-random.seed(67)
-np.random.seed(67)
+#random.seed(67)
+#np.random.seed(67)
+
 #political alignment is to go from +- 10 in all directions
 sizeX = 10
 sizeY = 10
 p = [np.array([-1, -1]), np.array([1, 1])]
+est_tally = [0, 0]
+e_size = 5
 
+fig, ax = matplotlib.pyplot.subplots()
 
 map = np.empty((sizeY, sizeX), dtype=object)
 for y in range(sizeY):
@@ -18,8 +24,15 @@ for y in range(sizeY):
         rot = np.random.uniform(-0.5, 0.5)
         map[y][x] = Cell(np.array([random.uniform(-9, 9), random.uniform(-9, 9)]), np.array([[stdx**2, stdx*stdy*rot], [stdy*stdx*rot, stdy**2]]), 500)
         map[y][x].assign_votes(p)
+        c = 'green'
+        if map[y][x].votes[0] > map[y][x].votes[1]:
+            c = 'orange'
+            est_tally[0] += 1
+        else:
+            est_tally[1] += 1
+        ax.add_patch(matplotlib.patches.Rectangle((x, y), 1, 1, color=c))
 
-print(map)
-texas = Cell(np.array([1.5, -2.0]), np.array([[4, 1.2], [1.2, 3]]), 500)
-texas.assign_votes(p)
-print(texas.votes)
+ax.set_ylim(sizeY)
+ax.set_xlim(sizeX)
+matplotlib.pyplot.savefig("districts.png")
+print('Saved!')
